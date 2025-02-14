643dacc3c844b6e3bed7c7fb236e67b894b73100
Eslam Ahmed <eslamahmedd171@gmail.com>
2025-02-14 16:09:13 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -15,8 +16,19 @@ int main() {
 
         // check the echo command
         const int ECHO_LEN = 5;
-        if (input.substr(0,  5) == "echo ")
+        const int TYPE_LEN = 5;
+        std::set<std::string> commands = {"exit", "echo", "type"};
+
+        if (input.substr(0,  ECHO_LEN) == "echo ")
             std::cout << input.substr(ECHO_LEN) << std::endl;
+        else if (input.substr(0, TYPE_LEN) == "type ") {
+            std::string command = input.substr(TYPE_LEN);
+            if (commands.find(command) != commands.end()){
+                std::cout << command << " is a shell builtin" << std::endl;
+            } else{
+                std::cout << command << ": not found" << std::endl;
+            }
+        }
         else
             std::cout << input << ": command not found" << std::endl;
     }
