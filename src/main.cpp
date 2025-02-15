d4f1a33c288e77fefc4785c0e516b4a8f31eb01c
Eslam Ahmed <eslamahmedd171@gmail.com>
2025-02-15 09:33:16 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <filesystem>
+#include <unistd.h>
 #include <set>
+namespace fs = std::filesystem;
+
+
+bool commandInDirectory(const std::string& command, const std::string& directory){
+    try {
+        for (const auto &entry: fs::directory_iterator(directory)) {
+            if (entry.path().filename() == command) {
+                return true;
+            }
+        }
+    } catch(const fs::filesystem_error& error) {
+        std:: cout << error.what() << std::endl;
+        return false;
+    }
+    return false;
+}
+
+std::string getCommandDirectory(const std::string& command, const char* path_env) {
+    std::stringstream stringStream(path_env);
+    std::string directory;
+    while (std::getline(stringStream, directory, ':')) {
+        if (commandInDirectory(command, directory)) {
+            fs::path fullPath = fs::path(directory) / command;
+            return fullPath.string();
+        }
+    }
+    return "";
+}
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -25,6 +55,9 @@ int main() {
             std::string command = input.substr(TYPE_LEN);
             if (commands.find(command) != commands.end()){
                 std::cout << command << " is a shell builtin" << std::endl;
+            }
+            else if (!getCommandDirectory(command, std::getenv("PATH")).empty()){
+                std::cout << command << " is " << getCommandDirectory(command, std::getenv("PATH")) << std::endl;
             } else{
                 std::cout << command << ": not found" << std::endl;
             }
